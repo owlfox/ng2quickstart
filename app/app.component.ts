926f1817f3bd4e243a7c92da0ad1900c4505df5a
tjctw <tsejen.tw@gmail.com>
2016-06-11 20:29:53 +0800

    2 way data binding

--- app/app.component.ts
+++ app/app.component.ts
@@ -7,13 +7,21 @@ export class Hero {
 //A [@Component decorator](https://angular.io/docs/ts/latest/quickstart.html#component-decorator) that tells Angular what template to use and how to create the component.
 @Component({
   selector: 'my-app',
-  template: '<h1>{{title}}</h1><h2>{{hero.name}} details!</h2>'
+  template:`
+    <h1>{{title}}</h1>
+    <h2>{{hero.name}} details!</h2>
+    <div><label>id: </label>{{hero.id}}</div>
+    <div>
+      <label>name: </label>
+      <input [(ngModel)]="hero.name" placeholder="name">
+    </div>
+    `
 })
 // Every Angular app has at least one root component, conventionally named AppComponent
 export class AppComponent {
-    title = 'Tour of Heroes';
-    hero: Hero = {
-      id: 1,
-      name: 'Windstorm'
-    };
+  title = 'Tour of Heroes';
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
 }
