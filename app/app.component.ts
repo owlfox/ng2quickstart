7261c830ce8c727ec7f58b796d4a155274454a56
tjctw <tsejen.tw@gmail.com>
2016-06-11 19:46:35 +0800

    refractor

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+export class Hero {
+  id: number;
+  name: string;
+}
 
 //A [@Component decorator](https://angular.io/docs/ts/latest/quickstart.html#component-decorator) that tells Angular what template to use and how to create the component.
 @Component({
   selector: 'my-app',
-  template: '<h1>{{title}}</h1><h2>{{hero}} details!</h2>'
+  template: '<h1>{{title}}</h1><h2>{{hero.name}} details!</h2>'
 })
 // Every Angular app has at least one root component, conventionally named AppComponent
 export class AppComponent {
     title = 'Tour of Heroes';
-    hero = 'Windstorm';
+    hero: Hero = {
+      id: 1,
+      name: 'Windstorm'
+    };
 }
